ffe4faa7885e9ab49a4b1fb272dc84328ac686df
Andy Patari <andy.patari@gmail.com>
2023-08-28 20:37:43 -0400

    add Switch and Route

--- src/App.js
+++ src/App.js
@@ -4,14 +4,29 @@ import { Button, Navbar, Nav } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import DiscBag from './DiscBag';
 import Header from './Header';
+import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
+import Collection from './Collection';
+import AddDisc from './AddDisc';
 
 function App() {
   return (
-    <div className="App">
+    <div >
 
       <Header />
 
-      <DiscBag />
+      <Switch>
+        <Route exact path="/">
+          <DiscBag />
+        </Route>
+        <Route exact path="/collection">
+          <Collection />
+        </Route>
+        <Route exact path="/new">
+          <AddDisc />
+        </Route>
+      </Switch>
+
+      
 
 
     </div>
