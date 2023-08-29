707915f48cf40ce5e06c14d28754faa4839401a9
Andy Patari <andy.patari@gmail.com>
2023-08-28 20:57:50 -0400

    add working links to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,43 @@
 import React from "react";
 import { Navbar, Nav } from "react-bootstrap";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 function Header() {
+
+    const linkStyles = {
+        textDecoration: 'none',
+        color:'white'
+    }
+
     return (
         <div>
         <Navbar className='bg-success text-white p-3 bg-opacity-75'>
             <Navbar.Brand className='fw-bolder fs-2' >Bag Builder</Navbar.Brand>
             <Nav className='p-2 fs-4 text-white'>
-                <Nav.Link className='text-white me-3' >View Bag</Nav.Link>
-                <Nav.Link className='text-white me-3' >Select Discs</Nav.Link>
-                <Nav.Link className='text-white me-3' >Add New Discs</Nav.Link>
+                <NavLink 
+                    exact to="/" 
+                    className='text-white me-3' 
+                    style={linkStyles} 
+                    activeStyle={{textDecoration: 'overline'}} 
+                >
+                    View Bag
+                </NavLink>
+                <NavLink 
+                    exact to="/Collection" 
+                    className='text-white me-3' 
+                    style={linkStyles} 
+                    activeStyle={{textDecoration: 'overline'}} 
+                >
+                    Select Discs
+                </NavLink>
+                <NavLink 
+                    to="/new" 
+                    className='text-white me-3'
+                    style={linkStyles} 
+                    activeStyle={{textDecoration: 'overline'}} 
+                >
+                    Add New Discs
+                </NavLink>
             </Nav>
       </Navbar>
         </div>
