1bf73a4b978a1cff719d7415c39c9fe576cb32ca
Andy Patari <andy.patari@gmail.com>
2023-08-30 12:42:22 -0400

    search function works

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,15 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DiscCard from "./DiscCard";
 import { Col } from "react-bootstrap";
 import SearchSort from "./SearchSort";
 
 function Collection({ discs, setDiscs }) {
+
+    const [searchText, setSearchText] = useState("")
+    const [sortBy, setSortBy] = useState("")
+
+    function handleSearchChange(e) {
+        setSearchText(e.target.value)
+    }
+
+    function handleSortChange(e) {
+        setSortBy(e.target.value)
+    }
+
+
+    const discsToDisplay = discs.filter(disc =>  disc.model.toLowerCase().includes(searchText.toLowerCase()))
+   
+
+    
+    console.log(searchText)
+
     return(
         <div>
-            <SearchSort />
+            <SearchSort searchText={searchText} sortBy={sortBy} onSearchChange={handleSearchChange} onSortChange={handleSortChange} />
             <div className="p-2 m-4 rounded-3 bg-primary bg-opacity-25 ">
                 <Col className="d-flex flex-wrap p-3 ">
-                    {discs.map(disc => {
+                    {discsToDisplay.map(disc => {
                 return ( 
                         <DiscCard disc={disc} key={disc.id} discs={discs} setDiscs={setDiscs} />  
                     )})}
--- src/components/SearchSort.js
+++ src/components/SearchSort.js
@@ -1,13 +1,7 @@
 import React, { useState } from "react";
 import { Form, Col, Row, Container } from "react-bootstrap";
 
-function SearchSort() {
-
-    const [searchText, setSearchText] = useState("")
-
-    function handleSearchChange(e) {
-        setSearchText(e.target.value)
-    }
+function SearchSort({ searchText, sortBy, onSearchChange, onSortChange}) {
 
     return (
         <div>
@@ -17,11 +11,11 @@ function SearchSort() {
                     
                         <Col xs lg="4">
                             <Form.Label htmlFor="searchText">Search discs by model name</Form.Label>
-                            <Form.Control type="text" onChange={handleSearchChange} id="searchText"  value={searchText} placeholder="-"></Form.Control>
+                            <Form.Control type="text" onChange={onSearchChange} id="searchText"  value={searchText} placeholder="-"></Form.Control>
                         </Col>
                         <Col md="auto">
                             <Form.Label>Sort discs by:</Form.Label>
-                            <Form.Select>
+                            <Form.Select value={sortBy} onChange={onSortChange}>
                                 <option value="">-</option>
                                 <option value="sDec">Speed, decreasing</option>
                                 <option value="sInc">Speed, increasing</option>
