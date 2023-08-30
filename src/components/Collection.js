a65cb8c05c51d446e4db4cfa5d8ac0801df1e9b6
Andy Patari <andy.patari@gmail.com>
2023-08-30 12:47:57 -0400

    create compare fns for sort

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import DiscCard from "./DiscCard";
 import { Col } from "react-bootstrap";
 import SearchSort from "./SearchSort";
@@ -18,10 +18,10 @@ function Collection({ discs, setDiscs }) {
 
 
     const discsToDisplay = discs.filter(disc =>  disc.model.toLowerCase().includes(searchText.toLowerCase()))
-   
+    discsToDisplay.sort(compareDecreasing)
 
     
-    console.log(searchText)
+    console.log(discsToDisplay)
 
     return(
         <div>
@@ -36,6 +36,18 @@ function Collection({ discs, setDiscs }) {
             </div>
         </div>
     )
+}
+
+function compareIncreasing(a,b) {
+    if (a.speed>b.speed) { return 1}
+    if (a.speed<b.speed) { return -1}
+    return 0
+}
+
+function compareDecreasing(a,b) {
+    if (a.speed>b.speed) { return -1}
+    if (a.speed<b.speed) { return 1}
+    return 0
 }
 
 export default Collection
