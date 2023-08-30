a3852a65a07a45562d1f22c4386fafddbbd75c8c
Andy Patari <andy.patari@gmail.com>
2023-08-29 20:39:49 -0400

    control form for adding discs

--- src/components/AddDisc.js
+++ src/components/AddDisc.js
@@ -16,6 +16,16 @@ function AddDisc() {
 
     const invalidForm = true
 
+    function handleChange(e) {
+        const name = e.target.name
+        const value = e.target.value
+        setFormData({
+            ...formData, 
+            [name]:value
+        })
+
+    }
+
     return (
         <div>
             <Form className="m-4">
@@ -24,14 +34,14 @@ function AddDisc() {
                 
                     <Form.Group className="m-3 ">
                         <Form.Label>Disc Model</Form.Label>
-                        <Form.Control type="text" name="model" placeholder="Model" value={formData.model} />
+                        <Form.Control type="text" name="model" placeholder="Model" value={formData.model} onChange={handleChange} />
                     </Form.Group>
                 
                 </Col>
                 <Col>
                     <Form.Group className="m-3 ">
                     <Form.Label>Type of disc</Form.Label>
-                        <Form.Select name="type" value={formData.type}>
+                        <Form.Select name="type" value={formData.type} onChange={handleChange} >
                             <option value="">Select a type</option>
                             <option value="distance">Distance driver</option>
                             <option value="fairway">Fairway driver</option>
@@ -43,7 +53,7 @@ function AddDisc() {
                 <Col>
                     <Form.Group className="m-3 ">
                         <Form.Label>Image URL</Form.Label>
-                        <Form.Control type="text"  placeholder="Image" value={formData.image} />
+                        <Form.Control type="text"  placeholder="Image" name="image" value={formData.image} onChange={handleChange} />
                     </Form.Group>
                 </Col>
             </Row>
@@ -51,25 +61,25 @@ function AddDisc() {
                 <Col>
                     <Form.Group className="m-3 ">
                         <Form.Label>Speed</Form.Label>
-                        <Form.Control type="number" step={0.5} placeholder="Speed" value={formData.speed} />
+                        <Form.Control type="number" step={0.5} placeholder="Speed" name="speed" value={formData.speed} onChange={handleChange} />
                     </Form.Group>
                 </Col>
                 <Col>
                     <Form.Group className="m-3 ">
                         <Form.Label>Glide</Form.Label>
-                        <Form.Control type="number" step={0.5} placeholder="Glide" value={formData.glide} />
+                        <Form.Control type="number" step={0.5} placeholder="Glide" name="glide" value={formData.glide} onChange={handleChange} />
                     </Form.Group>
                 </Col>
                 <Col>
                     <Form.Group className="m-3 ">
                         <Form.Label>Turn</Form.Label>
-                        <Form.Control type="number" step={0.5} placeholder="Turn" value={formData.turn} />
+                        <Form.Control type="number" step={0.5} placeholder="Turn" name="turn" value={formData.turn} onChange={handleChange} />
                     </Form.Group>
                 </Col>
                 <Col>
                     <Form.Group className="m-3 ">
                         <Form.Label>Fade</Form.Label>
-                        <Form.Control type="number" step={0.5} placeholder="Fade" value={formData.fade} />
+                        <Form.Control type="number" step={0.5} placeholder="Fade" name="fade" value={formData.fade} onChange={handleChange} />
                     </Form.Group>
                 </Col>
             </Row>
