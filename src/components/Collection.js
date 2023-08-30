4ab2e18959ed3f0c6bcc06aad5c221d40d994402
Andy Patari <andy.patari@gmail.com>
2023-08-30 12:03:20 -0400

    create searchsort file

--- src/components/Collection.js
+++ src/components/Collection.js
@@ -1,10 +1,12 @@
 import React from "react";
 import DiscCard from "./DiscCard";
 import { Col } from "react-bootstrap";
+import SearchSort from "./SearchSort";
 
 function Collection({ discs, setDiscs }) {
     return(
         <div>
+            <SearchSort />
             <div className="p-2 m-4 rounded-3 bg-primary bg-opacity-25 ">
                 <Col className="d-flex flex-wrap p-3 ">
                     {discs.map(disc => {
---
+++ src/components/SearchSort.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Form, Col, Row } from "react-bootstrap";
+
+function SearchSort() {
+    return (
+        <div>
+            <h2>searchsort</h2>
+        </div>
+    )
+}
+
+export default SearchSort
